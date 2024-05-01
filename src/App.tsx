257fd23d0920275d5530bbfc02b28c13c2a79eb0
Santiago Crippa <santicrippa@gmail.com>
2024-05-01 14:52:19 -0300

    Finishing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 // eslint-disable-next-line import/no-absolute-path
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react'
 
 export default function App() {
-  const [count, setCount] = useState(0)
+  const [colour, setColour] = useState('')
+
+  const onclick = async () => {
+    const [tab] = await chrome.tabs.query({ active: true })
+    chrome.scripting.executeScript<string[], unknown>({
+      target: { tabId: tab.id! },
+      args: [colour],
+      func: (colour) => {
+        document.body.style.backgroundColor = colour
+      }
+    })
+  }
 
   return (
     <>
@@ -17,18 +28,17 @@ export default function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>My Extension</h1>
       <div className="card">
-        <button onClick={() => { setCount((count) => count + 1) }}>
-          count is {count}
+        <input
+          type="color"
+          value={colour}
+          className='input'
+          onChange={(e) => { setColour(e.currentTarget.value) }} />
+        <button className='btn' onClick={() => { onclick() }}>
+          Click to change background.
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
